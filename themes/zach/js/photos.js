066c9917f995c771d32c54dc5f77544500919a32
zpalexander <zpalexander@gmail.com>
2014-04-13 14:18:58 -0400

    photos go fullscreen onclick

--- themes/zach/js/photos.js
+++ themes/zach/js/photos.js
@@ -1,16 +1,43 @@
 (function($) {
 
 	var initIsotope = function() {
-		$('.photo-content').isotope({
+		$( '.photo-content' ).isotope({
   			// options
   			itemSelector : '.photo',
-  			layoutMode : 'fitRows'
+  			layoutMode : 'fitRows',
   			transitionDuration: '1.0s',
 		});
 	};
 
+	var openFullScreenImage = function() {
+
+	};
+
+	var closeFullScreenImage = function() {
+
+	}
+
+
 	$(window).load( function() {
 		initIsotope();
+
+		// Open fullscreen photo onclick
+		$( '.photo' ).click(function() {
+			var imageSource = $('img', this).attr('src');
+			var imageTag = '<img src="'+imageSource+'"></img>';
+			$( '.fullscreen-photo')
+				.append( imageTag )
+				.fadeIn(900);
+			$('body').animate({scrollTop:$('.fullscreen-photo img').offset().top},500)
+
+
+		});
+
+		// Close fullscreen photo onclick
+		$( '.fullscreen-photo' ).click(function() {
+			$( this ).fadeOut(900);
+			$( '.fullscreen-photo img' ).remove();
+		});
 	});
 
 })(jQuery);
--- themes/zach/page-photos.php
+++ themes/zach/page-photos.php
@@ -8,37 +8,36 @@ get_header(); ?>
 
 <section id="primary" class="content-area">
 	<div id="content" class="photo-content" role="main">
-		This will be the photos page!
 		<div class="photo-container">
-			<div class="photo">
-				<img src="http://www.fillmurray.com/300/200">
-			</div>
-			<div class="photo">
-				<img src="http://www.fillmurray.com/300/200">
-			</div>
-			<div class="photo">
-				<img src="http://www.fillmurray.com/300/200">
-			</div>
-			<div class="photo">
-				<img src="http://www.fillmurray.com/300/200">
-			</div>
-			<div class="photo">
-				<img src="http://www.fillmurray.com/300/200">
-			</div>
-			<div class="photo">
-				<img src="http://www.fillmurray.com/300/200">
-			</div>
-			<div class="photo">
-				<img src="http://www.fillmurray.com/300/200">
-			</div>
-			<div class="photo">
-				<img src="http://www.fillmurray.com/300/200">
-			</div>
-			<div class="photo">
-				<img src="http://www.fillmurray.com/300/200">
-			</div>
+		<?php 
+			$query_args = array(
+				'posts_per_page'   => 12,
+				'offset'           => 0,
+				'category'         => '',
+				'orderby'          => 'post_date',
+				'order'            => 'DESC',
+				'include'          => '',
+				'exclude'          => '',
+				'meta_value'       => '',
+				'post_type'        => 'photo',
+				'post_status'      => 'publish',
+				'suppress_filters' => true ); 
+			$photos_array = get_posts($query_args);
+
+			if ($photos_array) {
+				foreach($photos_array as $photo_post) {
+		?>
+					<div class="photo">
+						<img src="<?php echo get_post_meta($photo_post->ID, '_zach_photo_file', TRUE) ?>">
+					</div>
+		<?php
+				}
+			}
+		?>
+			
 		</div>
 	</div>
+	<div class="fullscreen-photo"></div>
 </section>
 
 <?php
