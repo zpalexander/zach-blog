ab4ccd6ec73f84606d48d46caeb3b1763709ed22
zpalexander <zpalexander@gmail.com>
2014-05-10 16:03:04 -0400

    business card wireframed

--- themes/zach/js/custom/about.js
+++ themes/zach/js/custom/about.js
@@ -1,11 +1,17 @@
 (function($) {
 
+	var setContainers = function() {
+		var windowHeight = $( window ).height();
+		$( '.business-card' ).height(windowHeight);
+	};
+
 	var parallax = function() {
 	    var scrolled = $(window).scrollTop();
 	    $('.about-background').css('top', -(scrolled * 0.2) + 'px');
-	}
+	};
 
 	$(window).load( function() {
+		setContainers();
 		$( window ).scroll(parallax);	
 	});
 
