8f704e1a7258ec907199f9aa2136cf72610f52bd
zpalexander <zpalexander@gmail.com>
2014-05-11 11:43:36 -0400

    webkit animated down arrow on business card, parallax cleaned up

--- themes/zach/js/custom/about.js
+++ themes/zach/js/custom/about.js
@@ -3,13 +3,19 @@
 	var setContainers = function() {
 		var windowHeight = $( window ).height();
 		$( '.business-card' ).height( (windowHeight-55) );
-		$( '.primary-skills' ).height( (windowHeight-65) );
+		$( '.primary-skills' ).height( (windowHeight-65) ).css('margin-top', windowHeight);
+	};
+
+	var fadeInContent = function() {
+		$( '.page-id-54 .site-content' ).fadeIn( 1000 );
 	};
 
 	var fadeInBusinessCard = function() {
 		$( '.business-card img' ).animate({opacity: 1}, 2000);
-		$( '.business-card h2' ).delay( 2250 ).fadeIn( 1500 );
-		$( '.business-card p').delay( 3750 ).fadeIn( 1500 );
+		$( '.business-card h2' ).delay( 2000 ).fadeIn( 1500 );
+		$( '.business-card .self-summary').delay( 3500 ).fadeIn( 1500 );
+		$( '.down-arrow' ).delay( 6000 ).fadeIn( 500 );
+		$( '.arrow-instructions').delay( 6000 ).fadeIn( 500 )
 	}
 
 	var parallax = function() {
@@ -19,17 +25,18 @@
 	    $('.about-background').css('top', -(scrolled * 0.1) + 'px');
 
 	    // After interests, hide first image and show second
-	    if ( scrolled > windowHeight * 1.75) {
+	    if ( scrolled > windowHeight * 1.9) {
 	    	$( '.overlooking-water').css('z-index', '1');
 	    	$( '.edc' ).css('z-index', '2');	
 	    }
-	    else if ( (scrolled < windowHeight * 1.75) && ( $( '.edc' ).css('z-index') == 2 ) ) {
+	    else if ( (scrolled < (windowHeight * 1.9) ) && ( $( '.edc' ).css('z-index') == 2 ) ) {
 	    		$( '.overlooking-water').css('z-index', '2');
 	    		$( '.edc' ).css('z-index', '1');
 	    }
 	};
 
 	$(window).load( function() {
+		fadeInContent();
 		setContainers();
 		fadeInBusinessCard();
 		$( window ).scroll(parallax);	
--- themes/zach/page-about.php
+++ themes/zach/page-about.php
@@ -13,8 +13,10 @@ get_header(); ?>
 			<div class="business-card">
 				<img src="<?php echo get_stylesheet_directory_uri() . '/images/avatar.png';?>">
 				<h2>My name is Zach Alexander</h2>
-				<p>I'm a full-stack web developer</p>
-				<p>I live in Brooklyn, NY</p>
+				<p class="self-summary">I'm a full-stack web developer</p>
+				<p class="self-summary">I live in Brooklyn, NY</p>
+				<div class="down-arrow"></div>
+				<p class="arrow-instructions">Click or scroll to continue</p>
 			</div>
 
 			<div class="primary-skills">
@@ -22,11 +24,9 @@ get_header(); ?>
 			</div>
 
 			<div class="experience">
-				Saucer cultivar flavour java, café au lait, fair trade eu aromatic qui, strong crema flavour et, mocha to go, sweet grinder macchiato roast kopi-luwak extraction single origin doppio. Extra fair trade in, iced french press extraction lungo milk mocha decaffeinated coffee half and half sugar cinnamon foam bar viennese. Filter turkish, aftertaste, viennese as bar, cultivar variety, eu instant trifecta filter as crema breve espresso froth. Et chicory robusta kopi-luwak, in cultivar coffee, aged seasonal, as, sit iced froth filter dripper rich. Qui, seasonal that, variety french press that, espresso frappuccino, black single origin body grinder cortado frappuccino, sugar, milk froth spoon aromatic siphon sweet. Cup aftertaste and ut half and half variety dark, in instant carajillo crema wings, rich grinder galão viennese dark cup extra shop froth. Doppio, americano a est, siphon cream, lungo fair trade breve instant a body redeye plunger pot, extra so, mug at cortado white instant cup organic caramelization.
 			</div>
 
 			<div class="notable-projects">
-				Affogato viennese body caramelization trifecta, rich grinder half and half flavour decaffeinated coffee cultivar qui americano whipped cultivar ristretto body seasonal. Cream whipped arabica id dark milk id aftertaste, caramelization medium pumpkin spice cup percolator carajillo. Macchiato cultivar café au lait brewed cinnamon est french press half and half, viennese, wings est in, viennese as grinder single shot trifecta filter body crema rich white aroma café au lait. Mocha skinny robust, instant aroma body instant as grinder body saucer extra, grinder, mazagran coffee crema doppio, crema coffee bar roast dark cinnamon. So cream java cappuccino frappuccino sit barista whipped, aftertaste as fair trade, carajillo barista, turkish coffee crema, cream, kopi-luwak, mazagran wings beans id chicory. Organic macchiato, redeye qui aromatic coffee foam, cultivar est spoon blue mountain shop viennese. Pumpkin spice decaffeinated macchiato cortado that strong cinnamon, aroma a sit percolator to go cultivar.	
 			</div>
 
 			<div class="hobbies">
