f46dcb9f2ad54bcc05312bc97af998313352425a
Zach Alexander <zach@elephantventures.com>
2014-12-07 20:22:48 +0800

    clean up messy template syntax for single page

--- themes/zach/single.php
+++ themes/zach/single.php
@@ -10,33 +10,27 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-			?>
+			<?php while ( have_posts() ) : the_post(); ?>
 			<div class="blog-post-wrapper">
 				<h1><?php the_title(); ?></h1>
 				<p class="blog-post-date"><?php the_date(); ?></p>
-				<?php
-					$categories = wp_get_post_categories(get_the_ID());
-					if ( $categories[0] != 'Uncategorized' ) {
-						echo '<p class="blog-tags">';
-							echo '<img src="' . get_stylesheet_directory_uri() . '/images/tag-icon.png ?>">';
-							foreach ( $categories as $category ) {
-								$filter_url = get_site_url() . '/blog?cat=' . $category;
-								echo "<a href='$filter_url'>";
-								echo '<span class="blog-post-category">' . get_category($category)->name . '</span>';
-								echo '</a>';
-							}
-						echo '</p>';
-					}
-				?>
+				<?php $categories = wp_get_post_categories(get_the_ID()); ?>
+				<?php if ( $categories[0] != 'Uncategorized' ) : ?>
+					<p class="blog-tags">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tag-icon.png">
+						<?php foreach ( $categories as $category ) : ?>
+							<?php $filter_url = get_site_url() . '/blog?cat=' . $category; ?>
+							<a href='<?php echo $filter_url ?>'>
+							<span class="blog-post-category"><?php echo get_category($category)->name; ?></span>
+							</a>
+						<?php endforeach; ?>
+					</p>
+				<?php endif; ?>
 				<img class="blog-post-image" src="<?php echo blog_post_image($post->ID) ?>">
 				<div class="blog-post-content">
 					<?php the_content(); ?>
 				</div>
-
-				<?php endwhile; ?>
+			<?php endwhile; ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
