d28ba860318525fc3e59a083230c028c8ae561aa
Zach Alexander <zach@elephantventures.com>
2014-12-07 19:41:56 +0800

    fix typo in new css task

--- themes/zach/Gruntfile.js
+++ themes/zach/Gruntfile.js
@@ -38,6 +38,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default', ['watch']);
-	grunt.registerTask('css', [sass]);
+	grunt.registerTask('css', ['sass']);
 	grunt.registerTask('js', ['uglify']);
 }
