69e6de8067583c6d139c53af161be05e7b8dc27f
Zach Alexander <zpalexander@gmail.com>
2015-09-19 20:06:56 -0400

    Update place where i live

--- themes/zach/page-about.php
+++ themes/zach/page-about.php
@@ -13,7 +13,7 @@ get_header(); ?>
 				<img src="<?php echo get_stylesheet_directory_uri() . '/images/avatar.png';?>">
 				<h2>My name is Zach Alexander</h2>
 				<p class="self-summary">I'm a full-stack web developer</p>
-				<p class="self-summary">I live in Beijing, China</p>
+				<p class="self-summary">I live in Brooklyn, NY</p>
 				<a href="<?php echo esc_url( home_url( 'wp-content/themes/zach/images/zach-alexander-resume.pdf' ) ); ?>" rel="cv">
 					<div class="download-cv-button">Download My CV</div>
 				</a>
