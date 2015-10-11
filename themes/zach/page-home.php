d3d81adb83bfdacacf3c8840c18da2bb64e14422
Zach Alexander <zpalexander@gmail.com>
2015-10-11 17:40:35 -0400

    Add instagram icon to homepage

--- themes/zach/page-home.php
+++ themes/zach/page-home.php
@@ -16,12 +16,17 @@ get_header(); ?>
 			</li>
 			<li>
 			<a href="https://www.linkedin.com/pub/zachary-alexander/85/44/3b4">
-				<img class="social-media-link linkedin" src="<?php echo get_stylesheet_directory_uri() ?>/images/sm-icons/linkedin-96.png ?>" >	
+				<img class="social-media-link linkedin" src="<?php echo get_stylesheet_directory_uri() ?>/images/sm-icons/linkedin-96.png ?>" >
 			</a>
 			</li>
 			<li>
+				<a href="http://www.instagram.com/zpalexander">
+					<img class="social-media-link instagram" src="<?php echo get_stylesheet_directory_uri() ?>/images/sm-icons/instagram-96.png ?>" >
+				</a>
+			</li>
+			<li>
 			<a href="mailto:zpalexander@gmail.com">
-				<img class="social-media-link email" src="<?php echo get_stylesheet_directory_uri() ?>/images/sm-icons/email-96.png ?>" >	
+				<img class="social-media-link email" src="<?php echo get_stylesheet_directory_uri() ?>/images/sm-icons/email-96.png ?>" >
 			</a>
 			</li>
 		</ul>
