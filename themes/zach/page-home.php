0d8d7453553e1a3bac0be4dd1d7d242bc73569b1
Zach Alexander <zpalexander@gmail.com>
2015-10-15 17:02:27 -0400

    Fix broken social media icon paths

--- themes/zach/page-home.php
+++ themes/zach/page-home.php
@@ -11,22 +11,22 @@ get_header(); ?>
 		<ul class="social-media-container list-reset">
 			<li>
 			<a href="https://www.twitter.com/zpalexander">
-				<img class="social-media-link twitter" src="<?php echo get_stylesheet_directory_uri() ?>/images/sm-icons/twitter-96.png ?>" >
+				<img class="social-media-link twitter" src="<?php echo get_stylesheet_directory_uri() ?>/images/sm-icons/twitter-96.png" >
 			</a>
 			</li>
 			<li>
 			<a href="https://www.linkedin.com/pub/zachary-alexander/85/44/3b4">
-				<img class="social-media-link linkedin" src="<?php echo get_stylesheet_directory_uri() ?>/images/sm-icons/linkedin-96.png ?>" >
+				<img class="social-media-link linkedin" src="<?php echo get_stylesheet_directory_uri() ?>/images/sm-icons/linkedin-96.png" >
 			</a>
 			</li>
 			<li>
 				<a href="http://www.instagram.com/zpalexander">
-					<img class="social-media-link instagram" src="<?php echo get_stylesheet_directory_uri() ?>/images/sm-icons/instagram-96.png ?>" >
+					<img class="social-media-link instagram" src="<?php echo get_stylesheet_directory_uri() ?>/images/sm-icons/instagram-96.png" >
 				</a>
 			</li>
 			<li>
 			<a href="mailto:zpalexander@gmail.com">
-				<img class="social-media-link email" src="<?php echo get_stylesheet_directory_uri() ?>/images/sm-icons/email-96.png ?>" >
+				<img class="social-media-link email" src="<?php echo get_stylesheet_directory_uri() ?>/images/sm-icons/email-96.png" >
 			</a>
 			</li>
 		</ul>
